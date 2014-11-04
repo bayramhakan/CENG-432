8ea4c453f6cf70f50441c57abea953bbb029c759
bayramhakan <bayramhakankocatepe@gmail.com>
2014-11-04 20:15:35 +0200

    Update simple.scala

--- HW-01/simple.scala
+++ HW-01/simple.scala
@@ -1,2 +1,10 @@
-object simple (){
+object Simple extends App (){
+  
+  val arrayNumbers = Array(10,25,30)
+  arrayNumbers.foreach((i: Int) {
+  
+   if(i % 2 == 0) println(i * 2)
+   else if(i % 2 == 1) println(i * 3)
+  
+   })
 }

585195b143e0958a2123bde19651aa74ca15035f
bayramhakan <bayramhakankocatepe@gmail.com>
2014-11-13 23:59:08 +0200

    Update IyteMutableList.scala

--- HW-02/IyteMutableList.scala
+++ HW-02/IyteMutableList.scala
@@ -1,1 +1,21 @@
-/
+class IyteMutList {
+  var mutList: Array[Int] = new Array[Int](0); 
+
+  def add(val: Int): IyteMutList = {
+   
+  mutList = Array.concat(mutList, tArray); 
+  }
+  override def toString(): String = {
+    
+    if(num == 0) "";
+   
+    else  if (num < mutList.length) {
+        str += ",";
+      }
+    }
+    return str;
+  }
+}
+object IyteMutList extends IyteMutList{
+	  def apply = new IyteMutList()
+	}
